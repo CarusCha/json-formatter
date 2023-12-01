150f5c19b332cd4cab12f9ba598f31ae8c6e0576
caruscha <caruscha.hk@gmail.com>
2023-12-01 12:36:20 +0800

    add escaping button

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ textarea.value = JSON.stringify([
   {
     "ret": "0",
   }
-]);
+], null, 4);
 
 // textarea value to JSON object
 var setJSON = function () {
   try {
     var value = textarea.value;
     jsonObj = JSON.parse(value);
+    textarea.value = JSON.stringify(jsonObj, null, 4);
   }
   catch (err) {
     alert(err);
@@ -23,10 +24,21 @@ var setJSON = function () {
 // load default value
 setJSON();
 
+var unescapeBtn = document.querySelector("button.unescape");
 var loadJsonBtn = document.querySelector("button.load-json");
 var collapseBtn = document.querySelector("button.collapse");
 var expandBtn = document.querySelector("button.expand");
 var resetBtn = document.querySelector("button.reset");
+
+unescapeBtn.addEventListener("click", function () {
+  try {
+    var value = `"${textarea.value}"`;
+    textarea.value = JSON.parse(value);
+  }
+  catch (err) {
+    alert(err);
+  }
+});
 
 loadJsonBtn.addEventListener("click", function () {
   setJSON();
